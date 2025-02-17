100b0729a818a6712b2d5016fa41a8a3f21ac181
Julien Delsescaux <jdlxt@debian12>
2025-02-17 10:32:22 +0100

    test actions

--- .github/scripts/test.php
+++ .github/scripts/test.php
@@ -1,9 +1,26 @@
 <?php
 
-// $getRepostoryUrl = 'https://api.github.com/user/repos';
+$getRepostoryUrl = 'https://api.github.com/user/repos';
 
 $options = getopt('', ['token:']);
+$token = $options['token'];
 
-print_r(
-    $options
-);
+
+
+//curl -H "Authorization: token YOUR_PERSONAL_ACCESS_TOKEN" \
+//     "https://api.github.com/user/repos?visibility=all&affiliation=owner&per_page=100"
+
+
+$context = stream_context_create([
+    "http" => [
+        "header" => "User-Agent: PHP\n" .
+            "Authorization: token $token\n"
+    ]
+]);
+$json = file_get_contents($getRepostoryUrl, false, $context);
+$repositories = json_decode($json, true);
+
+print_r($repositories);
+
+
+
