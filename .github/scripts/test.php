7d55b742c31915766c3d73f49073111e51b9f8f1
Julien Delsescaux <jdlxt@debian12>
2025-02-17 10:55:32 +0100

    test actions

--- .github/scripts/test.php
+++ .github/scripts/test.php
@@ -3,25 +3,19 @@
 $getRepostoryUrl = 'https://api.github.com/user/repos';
 
 $options = getopt('', ['token:']);
+$token = $options['token'];
 
-print_r($options);
+$context = stream_context_create([
+    "http" => [
+        "header" => "User-Agent: PHP\r\n" .
+            "Authorization: token $token\r\n" .
+            "Accept: application/vnd.github.v3+json\r\n"
+    ]
+]);
+$json = file_get_contents($getRepostoryUrl, false, $context);
+$repositories = json_decode($json, true);
 
-print_r($_ENV);
-
-// $token = $options['token'];
-
-
-// $context = stream_context_create([
-//     "http" => [
-//         "header" => "User-Agent: PHP\r\n" .
-//             "Authorization: token $token\r\n" .
-//             "Accept: application/vnd.github.v3+json\r\n"
-//     ]
-// ]);
-// $json = file_get_contents($getRepostoryUrl, false, $context);
-// $repositories = json_decode($json, true);
-
-// print_r($repositories);
+print_r($repositories);
 
 
 
