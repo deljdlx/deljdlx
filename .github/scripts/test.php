67cac3aa38209e6e3ba18101be2815ac8ca7ffcf
Julien Delsescaux <jdlxt@debian12>
2025-02-17 10:50:32 +0100

    test actions

--- .github/scripts/test.php
+++ .github/scripts/test.php
@@ -6,25 +6,22 @@ $options = getopt('', ['token:']);
 
 print_r($options);
 
-$token = $options['token'];
+print_r($_ENV);
 
-echo strlen($token);
-echo "\n";
-echo substr($token, 0, 4);
-echo '-';
-echo substr($token, 4);
+// $token = $options['token'];
 
-$context = stream_context_create([
-    "http" => [
-        "header" => "User-Agent: PHP\r\n" .
-            "Authorization: token $token\r\n" .
-            "Accept: application/vnd.github.v3+json\r\n"
-    ]
-]);
-$json = file_get_contents($getRepostoryUrl, false, $context);
-$repositories = json_decode($json, true);
 
-print_r($repositories);
+// $context = stream_context_create([
+//     "http" => [
+//         "header" => "User-Agent: PHP\r\n" .
+//             "Authorization: token $token\r\n" .
+//             "Accept: application/vnd.github.v3+json\r\n"
+//     ]
+// ]);
+// $json = file_get_contents($getRepostoryUrl, false, $context);
+// $repositories = json_decode($json, true);
+
+// print_r($repositories);
 
 
 
